df6b4ae74c49802af86a87843a6897ecb3410738
Damien Roche <droche@gmail.com>
2015-11-22 23:20:42 +0000

    Set bind_addr for local eth1 - required

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,4 +9,7 @@
 
 node.default['consul']['config']['bootstrap_expect'] = node['consul-cluster']['config']['bootstrap_expect']
 node.default['consul']['config']['server'] = true
+node.default['consul']['config']['bind_addr'] = node['network']['interfaces']['eth1']['addresses'].keys[1]
+
+
 include_recipe 'consul::default'

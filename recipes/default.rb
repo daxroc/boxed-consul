5a7a93d1d2d2765c3d48f68aa112738b20eb66be
Damien Roche <droche@gmail.com>
2015-11-23 00:53:31 +0000

    Updated for reading encrypt from databag

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,6 +10,6 @@
 node.default['consul']['config']['bootstrap_expect'] = node['consul-cluster']['config']['bootstrap_expect']
 node.default['consul']['config']['server'] = true
 node.default['consul']['config']['bind_addr'] = node['network']['interfaces']['eth1']['addresses'].keys[1]
-
+node.default['consul']['config']['encrypt'] = data_bag_item('secrets', 'consul')['encrypt']
 
 include_recipe 'consul::default'
